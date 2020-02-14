aaf8814db05c9f061c5b1f611978166b3f81235c
NWRRobotics6945 <59284593+NWRRobotics6945@users.noreply.github.com>
2020-02-14 17:47:22 -0500

    Update OI.java

--- COB2-master/src/org/usfirst/frc/team6945/robot/OI.java
+++ COB2-master/src/org/usfirst/frc/team6945/robot/OI.java
@@ -5,38 +5,64 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc.team6945.robot;
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	//// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
-	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
+  public Joystick extremeJoystick;
+  public Joystick gamepad;
+  public Joystick arduinoLeonardo;
+  
+  public OI() {
+  
+  //Controllers
+  extremeJoystick = new Joystick(0); //santiago
 
-	// There are a few additional built in buttons you can use. Additionally,
-	// by subclassing Button you can create custom triggers and bind those to
-	// commands the same as any other Button.
+  gamepad = new Joystick(1); //not santiago
 
-	//// TRIGGERING COMMANDS WITH BUTTONS
-	// Once you have a button, it's trivial to bind it to a button in one of
-	// three ways:
+  //arduinoLeonardo = new Joystick(2); //also not santiago
 
-	// Start the command when the button is pressed and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenPressed(new ExampleCommand());
+  if (gamepad.getName().equals("Logitech Extreme 3D")) {
+    int newjoy = gamepad.getPort();
+    int newcon = extremeJoystick.getPort();
+    gamepad = new Joystick(newcon);
+    extremeJoystick = new Joystick(newjoy);
 
-	// Run the command while the button is being held down and interrupt it once
-	// the button is released.
-	// button.whileHeld(new ExampleCommand());
+    }
+/*
+  if (gamepad.getName().equals("Arduino Leonardo")) {
+    int newglove = gamepad.getPort();
+    int newcon = arduinoLeonardo.getPort();
+    gamepad = new Joystick(newcon);
+    arduinoLeonardo = new Joystick(newglove);
+    
+  }
 
-	// Start the command when the button is released and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenReleased(new ExampleCommand());
+  if(extremeJoystick.getName().equals("Arduino Leonardo")){
+    int newjoy = arduinoLeonardo.getPort();
+    int newglove = extremeJoystick.getPort();
+    extremeJoystick = new Joystick(newjoy);
+    arduinoLeonardo = new Joystick(newglove);
+  }
+
+*/
+
+ 
+}
+
+public void fixJoy() {
+  if (gamepad.getName().equals("Logitech Extreme 3D")) {
+    int newjoy = gamepad.getPort();
+    int newcon = extremeJoystick.getPort();
+    gamepad = new Joystick(newcon);
+    extremeJoystick = new Joystick(newjoy);
+  }
+}
 }
